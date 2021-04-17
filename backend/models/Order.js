6358b02a50e4c7065cebf792ba0656b12d048c56
Talgat Muratbekov <talgat2307@gmail.com>
2021-04-17 22:46:37 +0600

    #4 - Added Order model

---
+++ backend/models/Order.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const orderSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    town: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+  },
+);
+
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
